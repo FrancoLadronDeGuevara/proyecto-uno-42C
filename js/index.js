1a2f104bcf650e8e36a239919e7a5e5f434707eb
FrancoLadronDeGuevara <francoguevara93@gmail.com>
2025-04-17 16:01:25 -0300

    Se agregó session y local storage

--- js/index.js
+++ js/index.js
@@ -1,66 +1,80 @@
-let listaUsuariosPredeterminada = [ // Lista de usuarios predeterminada
-    {
-        email: "francoguevara@gmail.com",
-        password: "123456789"
-    },
-    {
-        email: "flaviaarmella@gmail.com",
-        password: "987654321"
-    },
-    {
-        email: "juanmedina@gmail.com",
-        password: "123789456"
-    }
-]
+let listaUsuariosPredeterminada = [
+  // Lista de usuarios predeterminada
+  {
+    email: "francoguevara@gmail.com",
+    password: "123456789",
+  },
+  {
+    email: "flaviaarmella@gmail.com",
+    password: "987654321",
+  },
+  {
+    email: "juanmedina@gmail.com",
+    password: "123789456",
+  },
+];
 
 const usuarios = JSON.parse(localStorage.getItem("listaUsuarios")) || []; //Si existe listaUsuarios en localStorage, la guardamos en la variable usuarios de lo contrario la inicializamos []
-const usuarioLogueado = JSON.parse(localStorage.getItem("usuarioLogueado")) || false; //Si existe usuarioLogueado en localStorage, la guardamos en la variable usuarioLogueado de lo contrario la inicializamos false
-const adminLogueado = JSON.parse(localStorage.getItem("adminLogueado")) || false; //Si existe adminLogueado en localStorage, la guardamos en la variable adminLogueado de lo contrario la inicializamos false
+const usuarioLogueado =
+  JSON.parse(localStorage.getItem("usuarioLogueado")) ||
+  JSON.parse(sessionStorage.getItem("usuarioLogueado")) ||
+  false; //Si existe usuarioLogueado en localStorage, la guardamos en la variable usuarioLogueado de lo contrario la inicializamos false
+const adminLogueado =
+  JSON.parse(localStorage.getItem("adminLogueado")) ||
+  JSON.parse(sessionStorage.getItem("adminLogueado")) ||
+  false; //Si existe adminLogueado en localStorage, la guardamos en la variable adminLogueado de lo contrario la inicializamos false
 
 //Recuperamos los botones
-const btnIniciarSesion = document.getElementById("btnIniciarSesion"); 
+const btnIniciarSesion = document.getElementById("btnIniciarSesion");
 const btnRegistrarse = document.getElementById("btnRegistrarse");
 const btnPanelControl = document.getElementById("btnPanelControl");
 const btnBienvenida = document.getElementById("btnBienvenida");
 const btnCerrarSesion = document.getElementById("btnCerrarSesion");
 
-if (usuarios.length === 0) { //Si la lista de usuarios esta vacia la inicializamos con la lista predeterminada
-    localStorage.setItem("listaUsuarios", JSON.stringify(listaUsuariosPredeterminada))
+if (usuarios.length === 0) {
+  //Si la lista de usuarios esta vacia la inicializamos con la lista predeterminada
+  localStorage.setItem(
+    "listaUsuarios",
+    JSON.stringify(listaUsuariosPredeterminada)
+  );
 }
 
-if(usuarioLogueado){ //Si el usuario esta logueado
-    btnIniciarSesion.classList.add("d-none"); //Ocultamos el boton de iniciar sesion
-    btnRegistrarse.classList.add("d-none"); //Ocultamos el boton de registrarse
-    btnBienvenida.classList.remove("d-none"); //Mostramos el boton de bienvenida
-    btnCerrarSesion.classList.remove("d-none"); //Mostramos el boton de cerrar sesion
+if (usuarioLogueado) {
+  //Si el usuario esta logueado
+  btnIniciarSesion.classList.add("d-none"); //Ocultamos el boton de iniciar sesion
+  btnRegistrarse.classList.add("d-none"); //Ocultamos el boton de registrarse
+  btnBienvenida.classList.remove("d-none"); //Mostramos el boton de bienvenida
+  btnCerrarSesion.classList.remove("d-none"); //Mostramos el boton de cerrar sesion
 }
 
-if(adminLogueado){ //Si el admin esta logueado
-    btnIniciarSesion.classList.add("d-none"); //Ocultamos el boton de iniciar sesion
-    btnRegistrarse.classList.add("d-none"); //Ocultamos el boton de registrarse
-    btnPanelControl.classList.remove("d-none"); //Mostramos el boton de panel de control
-    btnCerrarSesion.classList.remove("d-none"); //Mostramos el boton de cerrar sesion
+if (adminLogueado) {
+  //Si el admin esta logueado
+  btnIniciarSesion.classList.add("d-none"); //Ocultamos el boton de iniciar sesion
+  btnRegistrarse.classList.add("d-none"); //Ocultamos el boton de registrarse
+  btnPanelControl.classList.remove("d-none"); //Mostramos el boton de panel de control
+  btnCerrarSesion.classList.remove("d-none"); //Mostramos el boton de cerrar sesion
 }
 
 btnCerrarSesion.addEventListener("click", () => {
-    Swal.fire({
-        title: "Cerrar sesión?",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Si, cerrar!"
-      }).then((result) => {
-        if (result.isConfirmed) { //Si el usuario confirma
-          Swal.fire({
-            title: "Cerraste sesion con exito!",
-            icon: "success"
-          });
-          localStorage.removeItem("adminLogueado"); //Eliminamos el adminLogueado de localStorage
-          localStorage.removeItem("usuarioLogueado"); //Eliminamos el usuarioLogueado de localStorage
-          setTimeout(() => {
-            window.location.reload(); // Recargamos la pagina despues de 2 segundos
-          }, 2000);
-        }
+  Swal.fire({
+    title: "Cerrar sesión?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Si, cerrar!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      //Si el usuario confirma
+      Swal.fire({
+        title: "Cerraste sesion con exito!",
+        icon: "success",
       });
-    })
+      localStorage.removeItem("adminLogueado"); //Eliminamos el adminLogueado de localStorage
+      localStorage.removeItem("usuarioLogueado"); //Eliminamos el usuarioLogueado de localStorage
+      setTimeout(() => {
+        window.location.reload(); // Recargamos la pagina despues de 2 segundos
+      }, 2000);
+    }
+  });
+});
